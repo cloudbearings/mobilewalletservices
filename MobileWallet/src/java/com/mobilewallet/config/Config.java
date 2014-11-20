1479fcc3ccd4276f8e66bb6d8b224b4d1126448e
mobilewallet.android <mobilewallet.android@gmail.com>
2014-11-20 15:58:17 +0000

    Created project for MobileWallet app services

---
+++ MobileWallet/src/java/com/mobilewallet/config/Config.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mobilewallet.config;
+
+/**
+ *
+ * @author 123
+ */
+public class Config {
+
+    public static final String GCM_SERVER_ID = "AIzaSyBLAOakCF-8CvQFZ8nQjyxxs5yF0aS-BNw";
+    public static final String USER_ENCRYPTED_EXTENSION = "Wallet_Plusss.*#@!Config";
+}
