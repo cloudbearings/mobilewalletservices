76abbfeee8c7c615a92148d11a3f0e0a863f0328
mobilewallet.android <mobilewallet.android@gmail.com>
2015-01-03 10:39:52 +0000

    Created actions to show questions

--- MobileWalletAdmin/src/java/com/mobilewallet/admin/action/SubmitQuestionAction.java
+++ MobileWalletAdmin/src/java/com/mobilewallet/admin/action/SubmitQuestionAction.java
@@ -5,8 +5,11 @@
  */
 package com.mobilewallet.admin.action;
 
+import com.mobilewallet.admin.bo.QuestionBO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -19,6 +22,7 @@ public class SubmitQuestionAction extends org.apache.struts.action.Action {
 
     /* forward name="success" path="/jsp/submitQuestionResponse.jsp" */
     private static final String SUCCESS = "success";
+    private Log log = LogFactory.getLog(SubmitQuestionAction.class);
 
     /**
      * This is the action called from the Struts framework.
@@ -34,7 +38,16 @@ public class SubmitQuestionAction extends org.apache.struts.action.Action {
     public ActionForward execute(ActionMapping mapping, ActionForm form,
             HttpServletRequest request, HttpServletResponse response)
             throws Exception {
-        request.setAttribute("status", 1);
+        int submitted = QuestionBO.submitQuestion(request.getParameter("question"), request.getParameter("questionType"), request.getParameter("optiona"), request.getParameter("optionb"),
+                request.getParameter("optionc"), request.getParameter("optiond"), request.getParameter("answer"), request.getParameter("explanation"),
+                "Y");
+        String message;
+        if (submitted == 0) {
+            message = "Your question has been submitted successfully.";
+        } else {
+            message = "Internal server error";
+        }
+        request.setAttribute("status", message);
         return mapping.findForward(SUCCESS);
     }
 }
